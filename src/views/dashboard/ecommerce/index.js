e336ad54681cf2a0b891602cc989485813ef7ffd
shrusti2025 <shrusti.prolink@gmail.com>
2025-06-06 16:42:08 +0530

    remove categories

--- src/views/dashboard/ecommerce/index.js
+++ src/views/dashboard/ecommerce/index.js
@@ -2,11 +2,11 @@
 import "@styles/react/libs/charts/apex-charts.scss";
 import "@styles/base/pages/dashboard-ecommerce.scss";
 import { useEffect } from "react";
-import axiosInstance from "../../../auth/axiosInstance";
+// import axiosInstance from "../../../auth/axiosInstance";
 
 const EcommerceDashboard = () => {
   const fetch = async () => {
-    const res = await axiosInstance.get('/categories/categories')
+    // const res = await axiosInstance.get('/categories/categories')
   }
   useEffect(() => {
     fetch()
